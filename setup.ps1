640eddfbf3461ebaa74ef5e3fc45249777697e2d
chiefspecialk <sp3cialk@gmail.com>
2024-07-03 02:00:45 -0400

    revert profile creation and added scritpt to run again when WindowsTerminal Launches.

--- setup.ps1
+++ setup.ps1
@@ -119,8 +119,7 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
             $profilePath = "$env:userprofile\Documents\Powershell"
         }
         elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            Write-Host "The profile does not work properly with Powershell Desktop so profile will be installed for Core."
-            $profilePath = "$env:userprofile\Documents\Powershell"
+            $profilePath = "$env:userprofile\Documents\WindowsPowerShell"
         }
 
         if (!(Test-Path -Path $profilePath)) {
@@ -130,7 +129,7 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
         Invoke-RestMethod https://github.com/chiefspecialk/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -OutFile $PROFILE
         Write-Host "The profile @ [$PROFILE] has been created."
         Write-Host "If you want to make any personal changes or customizations, please do so at [$profilePath\Profile.ps1] as there is an updater in the installed profile which uses the hash to update the profile and will lead to loss of changes"
-       
+        
     }
     catch {
         Write-Error "Failed to create or update the profile. Error: $_"
@@ -228,6 +227,5 @@ catch {
 }
 
 if ($PSVersionTable.PSEdition -eq "Desktop"){
-    Start-Process wt.exe
+    Start-Process wt.exe -ArgumentList "-NoExit","-c","& {irm "https://github.com/chiefspecialk/powershell-profile/raw/main/setup.ps1" | iex}"
 }
-
