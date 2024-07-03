b11cda107394c2916aa1401abdd3b58258f86258
chiefspecialk <sp3cialk@gmail.com>
2024-07-03 12:14:32 -0400

    Removes profile from Windows Desktop version of Powershell

--- setup.ps1
+++ setup.ps1
@@ -229,5 +229,6 @@ catch {
 if ($PSVersionTable.PSEdition -eq "Desktop"){
     $scriptblock = {irm "https://github.com/chiefspecialk/powershell-profile/raw/main/setup.ps1" | iex}
     Start-Process wt.exe -ArgumentList "pwsh.exe", -command, $scriptblock
+    Remove-Item $PROFILE
     Write-Host "Profile should now be installed for Powershell 7 and usable in Windows Terminal. Enjoy!"
 }
